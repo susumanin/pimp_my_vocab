3988571257008aa25eeae30586cdcfab268f10b2
Aleksandr Aleksandrov <susumanin@gmail.com>
2015-06-09 23:07:37 +0300

    added abbility to insert words from file into DB

--- pimp_my_vocab.py
+++ pimp_my_vocab.py
@@ -1,5 +1,21 @@
 import argparse
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+Base = declarative_base()
+
+class WordsRank(Base):
+    __tablename__ = 'word_with_ranks'
+    rank = Column(Integer, primary_key=True)
+    word = Column(String)
+
+class WordStatus(Base):
+    __tablename__ = 'word_status'
+    word_rank = Column(Integer, primary_key=True)
+    word = Column(String)
+    status = Column(Integer)
 
 def load_words_from_text_to_sql(filename='', sqlConString='',
                                 numOfLines=0, printDebugMsg=False):
@@ -10,16 +26,31 @@ def load_words_from_text_to_sql(filename='', sqlConString='',
     fileWithWords = open(filename, 'r', encoding='utf-8')
     listOfWords = fileWithWords.readlines()
     lineIndex = 0
+    engine = create_engine('sqlite:///vocabDB_sqlite')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
     for line in listOfWords:
         lineIndex += 1
+        if lineIndex == 1:
+            continue
         splittedLine = line.split('\t')
-        wordRank = splittedLine[0]
+        wordRank = int(splittedLine[0])
         wordText = splittedLine[1].lower()
-        if printDebugMsg:
-            print('The word {} has rank {}'.format(wordText, wordRank))
+        result = session.query(WordsRank).filter(WordsRank.rank == wordRank).all()
+        if len(result) == 0:
+            new_word = WordsRank(rank=wordRank, word=wordText)
+            session.add(new_word)
+            if printDebugMsg:
+                print('The word {} has rank {} and added into the DB'.format(wordText, wordRank))
+        else:
+            if printDebugMsg:
+                print('The word {} is already in the DB'.format(wordText))
+
         if lineIndex == numOfLines:
             break
-
+    session.commit()
 
 def main():
     parser = argparse.ArgumentParser()
