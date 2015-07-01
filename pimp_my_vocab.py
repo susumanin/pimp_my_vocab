c7acbeed83a2a36c8ca59684e559742b259ed4f0
Aleksandr Aleksandrov <susumanin@gmail.com>
2015-07-01 23:45:26 +0300

    Add functuonal 'set_status' which allows to record which words user knows and which doesn't.  Please enter the commit message for your changes. Lines starting

--- pimp_my_vocab.py
+++ pimp_my_vocab.py
@@ -39,7 +39,7 @@ def load_words_from_text_to_sql(filename='', sqlConString='',
         wordText = splittedLine[1].lower()
         result = session.query(WordsRank).filter(WordsRank.rank == wordRank).all()
         if len(result) == 0:
-            new_word = WordsRank(rank=wordRank, word=wordText)
+            new_word = WordsRank(rank=wordRank, word=wordText, know_status = 0)
             session.add(new_word)
             if printDebugMsg:
                 print('The word {} has rank {} and added into the DB'.format(wordText, wordRank))
@@ -57,12 +57,41 @@ def show_stat(print_all=False):
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
     result = session.query(WordsRank).count()
+    count_of_know = session.query(WordsRank).filter(WordsRank.know_status == 1).count()
+    count_of_unknow = session.query(WordsRank).filter(WordsRank.know_status == 2).count()
     print('There are {} words in vocab'.format(result))
+    print('{} words you know'.format(count_of_know))
+    print('{} words you don\'t know'.format(count_of_unknow))
+    print('{} words without any status'.format(result - count_of_know - count_of_unknow))
     if print_all:
         result = session.query(WordsRank).all()
         for word in result:
             print('{} == rank == {}'.format(word.word, word.rank))
 
+
+def set_status_for_words(numOfWords):
+    engine = create_engine('sqlite:///vocabDB_sqlite')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    result = session.query(WordsRank).filter(WordsRank.know_status == 0)
+    if numOfWords != 0:
+        result = result.limit(numOfWords)
+    for word in result:
+        user_input = input('Do you know the word:   {}  ?'.format(word.word))
+        while True:
+            if user_input == 'y':
+                word.know_status = 1
+                break
+            elif user_input == 'n':
+                word.know_status = 2
+                break
+            else:
+                user_input = input('Enter y or n, please')
+    if result is None:
+        print('There is no words with unsetted status')
+    else:
+        session.commit()
 
 
 def main():
@@ -74,6 +103,7 @@ def main():
     parser.add_argument('--startFrom', type=int, help='''Number of lines to
                             read from file''')
     parser.add_argument('--printAll', type=int, help='''1 - print all words''')
+
     args = parser.parse_args()
     fileName = '/Users/mak/Documents/python_scripts/vocab/rank_word_count'
     sqlConString = ''
@@ -94,6 +124,8 @@ def main():
         load_words_from_text_to_sql(fileName, sqlConString,numOfLines, True, startFrom)
     elif args.action == 'show_stat':
         show_stat(printAll)
+    elif args.action == 'set_status':
+        set_status_for_words(numOfLines)
 
 
 
